2b2cd28b8a93f15243c151059097adc87487dc9e
Rohan <rohanaras@gmail.com>
2016-03-29 16:44:52 -0700

    flattened down preference file parsing in user request object

--- ispaq/concierge/user_requests.py
+++ ispaq/concierge/user_requests.py
@@ -12,35 +12,9 @@ import ispaq.utils.preferences as preferences
 
 from obspy import UTCDateTime
 
-
-# Dummy UserRequest object
-class DummyUserRequest(object):
-    def __init__(self):
-        """
-        Creates a dummy UserRequest object with sufficient properties for testing.
-
-        .. rubric:: Example
-
-        >>> my_request =  DummyUserRequest()
-        >>> my_request.sncl_patterns
-        ['US.OXF..*']
-        """
-        self.starttime = UTCDateTime("2002-04-20")
-        self.endtime = UTCDateTime("2002-04-21")
-        self.event_url = "IRIS"
-        self.station_url = "IRIS"
-        self.dataselect_url = "IRIS"
-        self.metric_names = ["num_gaps"]  # TODO:  to be removed
-        self.sncl_patterns = ["US.OXF..*"]
-        self.metric_dictionary = {'simple': {'basicStats': ['sample_min','sample_max'],
-                                             'gaps': ['num_gaps']} }
-        # More to be added as needed
-
-
-# Real UserRequest object
 class UserRequest(object):
     def __init__(self, requested_start_time, requested_end_time, requested_metric_set, requested_sncl_alias, pref_file,
-                 json_user_request=None):
+                 json_user_request=None, dummy_object=False):
         """
         Creates a UserRequest object.
 
@@ -50,7 +24,6 @@ class UserRequest(object):
         """
 
         #     Set fields from JSON if requested--------------------------------
-
         if json_user_request is not None:
             # Load json dictionary from file (or string)
             # TODO:  Test whether the `json` argument is a file or a string and
@@ -60,27 +33,65 @@ class UserRequest(object):
             # TODO:
             print("'json'_file argument not supported yet.")
 
+        #     Create dummy object if requested---------------------------------
+        elif dummy_object:
+            self.requested_metric_set = 'dummySetName'
+            self.requested_sncl_alias = 'dummySNCLAlias'
+            self.requested_start_time = UTCDateTime("2002-04-20")
+            self.requested_end_time = UTCDateTime("2002-04-21")
+            self.custom_metric_sets = {'dummySetName': ['sample_min', 'sample_rms']}
+            self.sncl_aliases = {'dummySNCLAlias': ['US.OXF..BHZ', 'IU.OXF..BH?']}
+            self.required_metric_set_functions = {'basicStats': 'dummySetName'}
+            self.dne_metrics = None
+            self.event_url = "IRIS"
+            self.station_url = "IRIS"
+            self.dataselect_url = "IRIS"
+
         #     Set fields from arguments       ---------------------------------
-
-        # desired metric set
-        self.metric_set_name = requested_metric_set
-        self.sncl_alias = requested_sncl_alias
-
-        # start and end times
-        self.requested_start_time = UTCDateTime(requested_start_time)
-
-        if requested_end_time is None:
-            self.requested_end_time = self.requested_start_time + (24 * 60 * 60)
         else:
-            self.requested_end_time = UTCDateTime(requested_end_time)
+            # desired metric set
+            self.requested_metric_set = requested_metric_set
+            self.requested_sncl_alias = requested_sncl_alias
 
-        #     Load preferences from file      ---------------------------------
+            # start and end times
+            self.requested_start_time = UTCDateTime(requested_start_time)
 
-        # custom metrics sets and sncls
-        self.custom_metric_sets, self.sncl_aliases = preferences.load(pref_file)
+            if requested_end_time:
+                self.requested_end_time = UTCDateTime(requested_end_time)
+            else:
+                self.requested_end_time = self.requested_start_time + (24 * 60 * 60)
 
-        # {irismustang function: custom metric set that they provide data for}, [sets that don't exist]
-        self.required_metric_set_functions, dne_sets = preferences.validate_metric_sets(self.custom_metric_sets)
+            #     Load preferences from file      ---------------------------------
+
+            # custom metrics sets and sncls
+            custom_metric_sets, custom_sncl = {}, {}
+            current = None
+            for line in pref_file:  # parse file
+                line = line.split('#')[0].strip() # remove comments
+                if line.lower() == "custom metric sets:":  # metric header
+                    current = 'm'
+                elif line.lower() == "sncl aliases:":  # sncl header
+                    current = 's'
+                elif current is not None:  # line following header
+                    entry = line.split(':')
+                    if len(entry) <= 1:  # empty line
+                        name, values = None, None
+                    else:  # non-empty line
+                        name = entry[0]
+                        values = entry[1].strip().split(',')
+                        values = [value.strip() for value in values]
+                        values = filter(None, values)  # remove empty strings
+                    if name is None:
+                        pass
+                    elif current == 'm':
+                        custom_metric_sets[name] = values
+                    elif current == 's':
+                        custom_sncl[name] = values
+
+            self.custom_metric_sets, self.sncl_aliases = custom_metric_sets, custom_sncl
+
+            # {irismustang function: custom metric set that they provide data for}, [metrics that don't exist]
+            self.required_metric_set_functions, self.dne_metrics = preferences.validate_metric_sets(self.custom_metric_sets)
 
     def json_dump(self):
         """
@@ -98,6 +109,25 @@ class UserRequest(object):
         properties_json = json.dumps(properties_dict)
         
         return properties_json
+
+    def __str__(self):
+        return "UserRequest Object @ %s: \n\t" \
+               "requested metric set: %s \n\t" \
+               "requested sncl alias: %s \n\t" \
+               "requested start time: %s \n\t" \
+               "requested end time: %s \n\t" \
+               "custom metric sets: %s \n\t" \
+               "requested metrics that dne: %s \n\t" \
+               "custom sncl aliases: %s \n\t" \
+               "required metric set functions: %s \n" % (hex(id(self)),
+                                                         self.requested_metric_set,
+                                                         self.requested_sncl_alias,
+                                                         self.requested_start_time,
+                                                         self.requested_end_time,
+                                                         self.custom_metric_sets,
+                                                         self.dne_metrics,
+                                                         self.sncl_aliases,
+                                                         self.required_metric_set_functions)
         
 
 if __name__ == '__main__':
