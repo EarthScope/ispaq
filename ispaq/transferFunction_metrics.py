2d941f532ef14f3a1a6f27b00660806d841f66c4
gillian-iris <gillian@iris.washington.edu>
2016-11-16 16:29:08 -0800

    minor edit to RESP warning

--- ispaq/transferFunction_metrics.py
+++ ispaq/transferFunction_metrics.py
@@ -114,7 +114,7 @@ def getTransferFunctionSpectra(st, sampling_rate, respDir=None):
                 if evalResp is not None:
                     break   # break early from loop if we found a result
         if evalResp is None:
-            raise EvalrespException('WARNING: No RESP file found at %s or %s for evalresp' % (localFile,localFile2))
+            raise EvalrespException('No RESP file found at %s or %s for evalresp' % (localFile,localFile2))
     else:    
         # calling the web service 
         evalResp = irisseismic.getEvalresp(network, station, location, channel, starttime,
