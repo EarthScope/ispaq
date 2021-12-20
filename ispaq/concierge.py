7200d437b944102d0fdb6f786edf8a4aaf115d5f
Hasan Issa <hasan.issa@nrcan-rncan.gc.ca>
2021-12-20 13:23:47 -0500

    Changed merge method to 1, and removed fpattern3 as its equal to the modified fpattern1

--- ispaq/concierge.py
+++ ispaq/concierge.py
@@ -299,11 +299,10 @@ class Concierge(object):
             self.fileDates = []
             for sncl_pattern in self.sncl_patterns:
                 matching_files = []
-                fpattern1 = '%s' % (sncl_pattern + '.[12][0-9][0-9][0-9].[0-9][0-9][0-9]')
+                fpattern1 = '%s' % (sncl_pattern + '.*[12][0-9][0-9][0-9].[0-9][0-9][0-9]')
                 fpattern2 = '%s' % (fpattern1 + '.[A-Z]')
-                fpattern3 = '%s' % (sncl_pattern + '.*[12][0-9][0-9][0-9].[0-9][0-9][0-9]')
                 for root, dirnames, fnames in os.walk(self.dataselect_url):
-                    for fname in fnmatch.filter(fnames, fpattern1) + fnmatch.filter(fnames, fpattern2), fnmatch.filter(fnames, fpattern3):
+                    for fname in fnmatch.filter(fnames, fpattern1) + fnmatch.filter(fnames, fpattern2):
                         matching_files.append(os.path.join(root,fname))
                 if (len(matching_files) == 0):
                     continue
@@ -624,9 +623,7 @@ class Concierge(object):
                                 tmp_sncl_pattern = os.path.splitext(sncl_pattern)[0]
                                 q = os.path.splitext(sncl_pattern)[1][1]
                                 
-                                fpattern1 = '%s' % (tmp_sncl_pattern + '.[12][0-9][0-9][0-9].[0-9][0-9][0-9]')
-                                fpattern3 = '%s' % (tmp_sncl_pattern + '.*[12][0-9][0-9][0-9].[0-9][0-9][0-9]')
-
+                                fpattern1 = '%s' % (tmp_sncl_pattern + '.*[12][0-9][0-9][0-9].[0-9][0-9][0-9]')
                                 if q.isalpha():
                                     fpattern2 = '%s' % (fpattern1 + '.' + q)
                                 else:
@@ -635,13 +632,12 @@ class Concierge(object):
                             else:
                                 fpattern1 = '%s' % (sncl_pattern + '.[12][0-9][0-9][0-9].[0-9][0-9][0-9]')
                                 fpattern2 = '%s' % (fpattern1 + '.[A-Z]')
-                                fpattern3 = '%s' % (sncl_pattern + '.*[12][0-9][0-9][0-9].[0-9][0-9][0-9]')
 
 
                             matching_files = []
 
                             for root, dirnames, fnames in os.walk(self.dataselect_url):
-                                for fname in fnmatch.filter(fnames, fpattern1) + fnmatch.filter(fnames, fpattern2) + fnmatch.filter(fnames, fpattern3):
+                                for fname in fnmatch.filter(fnames, fpattern1) + fnmatch.filter(fnames, fpattern2):
                                     file_year = int(fname.split('.')[4])
                                     file_day = int(fname.split('.')[5])
                                     file_date = (datetime.datetime(file_year, 1, 1) + datetime.timedelta(file_day - 1)).date()
@@ -842,13 +838,12 @@ class Concierge(object):
             # Subset based on locally available data ---------------------------
 #             if self.dataselect_client is None and metric is not "simple":
             if self.dataselect_client is None and metric != "simple":
-                fpattern1 = '%s.%s' % (_sncl_pattern,_starttime.strftime('%Y.%j'))
+                fpattern1 = '%s.*%s' % (_sncl_pattern,_starttime.strftime('%Y.%j'))
                 fpattern2 = '%s' % (fpattern1 + '.[A-Z]')
-                fpattern3 = '%s.*%s' % (_sncl_pattern,_starttime.strftime('%Y.%j'))
 
                 matching_files = []
                 for root, dirnames, fnames in os.walk(self.dataselect_url):
-                    for fname in fnmatch.filter(fnames, fpattern1) + fnmatch.filter(fnames, fpattern2)+ fnmatch.filter(fnames, fpattern3):
+                    for fname in fnmatch.filter(fnames, fpattern1) + fnmatch.filter(fnames, fpattern2):
                         file_year = int(fname.split('.')[4])
                         file_day = int(fname.split('.')[5])
                         file_date = (datetime.datetime(file_year, 1, 1) + datetime.timedelta(file_day - 1)).date()
@@ -993,12 +988,11 @@ class Concierge(object):
             
             if (nday == 1):
                 _sncl_pattern = self.get_sncl_pattern(network, station, location, channel)
-                fpattern1 = '%s.%s' % (_sncl_pattern,_starttime.strftime('%Y.%j'))
+                fpattern1 = '%s.*%s' % (_sncl_pattern,_starttime.strftime('%Y.%j'))
                 fpattern2 = '%s' % (fpattern1 + '.[A-Z]')
-                fpattern3 = '%s.*%s' % (_sncl_pattern,_starttime.strftime('%Y.%j'))
                 matching_files = []
                 for root, dirnames, fnames in os.walk(self.dataselect_url):
-                    for fname in fnmatch.filter(fnames, fpattern1) + fnmatch.filter(fnames, fpattern2)+ fnmatch.filter(fnames, fpattern3):
+                    for fname in fnmatch.filter(fnames, fpattern1) + fnmatch.filter(fnames, fpattern2):
                         matching_files.append(os.path.join(root,fname))
 
 #                 if (len(matching_files) == 0):
@@ -1036,7 +1030,7 @@ class Concierge(object):
                             
                         py_stream = obspy.read(filepath)
                         py_stream = py_stream.slice(_starttime, _endtime, nearest_sample=False)
-                        py_stream.merge(method=-1)
+                        py_stream.merge(method=1)
                         if (StrictVersion(obspy.__version__) < StrictVersion("1.1.0")): 
                             flag_dict = obspy.io.mseed.util.get_timing_and_data_quality(filepath)
                             act_flags = [0,0,0,0,0,0,0,0] # not supported before 1.1.0  
@@ -1123,12 +1117,11 @@ class Concierge(object):
                         end = _endtime
 
                     _sncl_pattern = self.get_sncl_pattern(network, station, location, channel)
-                    filename = '%s.%s' % (_sncl_pattern,_starttime.strftime('%Y.%j'))
+                    filename = '%s.*%s' % (_sncl_pattern,_starttime.strftime('%Y.%j'))
                     self.logger.debug("read local miniseed file for %s..." % filename)
-                    fpattern1 = self.dataselect_url + '/' + filename + '.[12][0-9][0-9][0-9].[0-9][0-9][0-9]'
+                    fpattern1 = self.dataselect_url + '/' + filename
                     fpattern2 = fpattern1 + '.[A-Z]'
-                    fpattern3 = self.dataselect_url + '/' + '%s.*%s.[12][0-9][0-9][0-9].[0-9][0-9][0-9]' % (_sncl_pattern,_starttime.strftime('%Y.%j'))
-                    matching_files = glob.glob(fpattern1) + glob.glob(fpattern2)+ glob.glob(fpattern3)
+                    matching_files = glob.glob(fpattern1) + glob.glob(fpattern2)
 		
                     if (len(matching_files) == 0):
                         err_msg = "No files found matching '%s'" % (fpattern1)
@@ -1153,7 +1146,7 @@ class Concierge(object):
                     if not inclusiveEnd:
                             _endtime = _endtime - 0.000001
                     py_stream = py_stream.slice(_starttime, _endtime, nearest_sample=False) 
-                    py_stream.merge(method=-1)
+                    py_stream.merge(method=1)
                     self.logger.debug(f'Successfully merged object {py_stream}')
                     # NOTE:  ObsPy does not store state-of-health flags with each stream.
                     if (StrictVersion(obspy.__version__) < StrictVersion("1.1.0")):
