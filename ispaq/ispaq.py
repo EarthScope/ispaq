939aa8b5a63c766152753718dd5e2f3c784a2644
Gillian Sharer <gillian@iris.washington.edu>
2022-12-08 11:25:33 -0800

    updated version to 3.1.1

--- ispaq/ispaq.py
+++ ispaq/ispaq.py
@@ -15,7 +15,7 @@ import subprocess
 from _ast import Try
 from numpy.random import sample
 
-__version__ = "3.1.0"
+__version__ = "3.1.1"
 
 # dictionary of currently defined ISPAQ metric groups and business logic
 # for comparison with R package IRISMustangMetrics/ISPAQUtils.R json
