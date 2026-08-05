6d767bf2e6c2d62d030d0c35f5dc42cd71199578
Laura Keyson <laura.keyson@earthscope.org>
2026-08-05 14:01:46 -0700

    update version

--- ispaq/ispaq.py
+++ ispaq/ispaq.py
@@ -13,8 +13,7 @@ import logging
 import numpy as np
 import subprocess
 
-
-__version__ = "3.4.0"
+__version__ = "3.4.1"
 
 # dictionary of currently defined ISPAQ metric groups and business logic
 # for comparison with R package IRISMustangMetrics/ISPAQUtils.R json
